dcb1a6d02859c8911b9b2ba029cd287349963331
pamodaaw <pamodaaw@gmail.com>
2018-06-29 12:23:00 +0530

    Declare the constants inside the class

--- component/src/main/java/org/wso2/extension/siddhi/execution/env/GetUserAgentPropertyFunction.java
+++ component/src/main/java/org/wso2/extension/siddhi/execution/env/GetUserAgentPropertyFunction.java
@@ -23,6 +23,7 @@ import org.wso2.siddhi.annotation.Parameter;
 import org.wso2.siddhi.annotation.ReturnAttribute;
 import org.wso2.siddhi.annotation.util.DataType;
 import org.wso2.siddhi.core.config.SiddhiAppContext;
+import org.wso2.siddhi.core.exception.SiddhiAppCreationException;
 import org.wso2.siddhi.core.exception.SiddhiAppRuntimeException;
 import org.wso2.siddhi.core.executor.ConstantExpressionExecutor;
 import org.wso2.siddhi.core.executor.ExpressionExecutor;
@@ -47,38 +48,40 @@ import java.util.Map;
         namespace = "env",
         description = "This function returns the value corresponding to a given property name in a given user agent",
         returnAttributes = @ReturnAttribute(
-                description = "Returned type will be string.",
+                description = "Property extracted from the user agent.",
                 type = {DataType.STRING}),
         parameters = {
                 @Parameter(name = "user.agent",
-                        description = "This specifies the user agent from which property is extracted.",
+                        description = "This specifies the user agent from which property will be extracted.",
                         type = {DataType.STRING}),
                 @Parameter(name = "property.name",
-                        description = "This specifies property name which should be extracted. Currently " +
-                                "supported properties are " + UserAgentConstants.BROWSER + ", "
-                                + UserAgentConstants.OPERATING_SYSTEM + " and " + UserAgentConstants.DEVICE + ".",
+                        description = "This specifies property name which should be extracted. " +
+                                "Supported property names are `'browser'`, `'os'` and `'device'`.",
                         type = {DataType.STRING})
         },
         examples = {
                 @Example(
-                        syntax = "define stream userAgentStream (userAgent string);\n" +
-                                "from userAgentStream env:getUserAgentProperty(userAgent, \"browser\") as " +
-                                "FunctionOutput \n" +
-                                "insert into outputStream;",
-                        description = "This query returns browser name of the userAgent from userAgentStream as " +
-                                "FunctionOutput to the outputStream"
+                        syntax = "define stream UserAgentStream (userAgent string);\n" +
+                                "from UserAgentStream \n" +
+                                "select env:getUserAgentProperty(userAgent, \"browser\") as " +
+                                "functionOutput \n" +
+                                "insert into OutputStream;",
+                        description = "This query returns browser name of the userAgent from UserAgentStream as " +
+                                "`functionOutput` to the OutputStream"
                 )
         }
 )
 public class GetUserAgentPropertyFunction extends FunctionExecutor {
 
-    /**
-     * The initialization method for GetUserAgentPropertyFunction,
-     * this method will be called before the other methods.
-     *
-     * @param attributeExpressionExecutors the executors of each function parameter.
-     * @param siddhiAppContext             the context of the execution plan.
-     */
+    private static final String BROWSER = "browser";
+    private static final String OPERATING_SYSTEM = "os";
+    private static final String DEVICE = "device";
+    private final List<String> listOfProperties = Arrays.asList(DEVICE, OPERATING_SYSTEM, BROWSER);
+    private Parser uaParser;
+    private String propertyName;
+
+    @Override
+
     protected void init(ExpressionExecutor[] attributeExpressionExecutors, ConfigReader configReader, SiddhiAppContext
             siddhiAppContext) {
 
@@ -87,109 +90,72 @@ public class GetUserAgentPropertyFunction extends FunctionExecutor {
             Attribute.Type typeofUserAgentAttribute = attributeExpressionExecutors[0].getReturnType();
             if (typeofUserAgentAttribute != Attribute.Type.STRING) {
                 throw new SiddhiAppValidationException("Invalid parameter type found " +
-                        "for the argument user.agent of getUserAgentProperty() function, " +
+                        "for the first argument 'user.agent' of getUserAgentProperty() function, " +
                         "required " + Attribute.Type.STRING +
                         ", but found " + typeofUserAgentAttribute.toString());
             }
             if (attributeExpressionExecutors[1] instanceof ConstantExpressionExecutor) {
-                String propertyName = ((ConstantExpressionExecutor) attributeExpressionExecutors[1])
+                propertyName = ((ConstantExpressionExecutor) attributeExpressionExecutors[1])
                         .getValue().toString();
-                List<String> listOfProperties = Arrays.asList(UserAgentConstants.DEVICE, UserAgentConstants
-                        .OPERATING_SYSTEM, UserAgentConstants.BROWSER);
+
                 if (!listOfProperties.contains(propertyName.toLowerCase(Locale.ENGLISH))) {
                     throw new SiddhiAppValidationException("Invalid parameter found " +
-                            "for the argument property.name of getUserAgentProperty() function, " +
-                            "required " + listOfProperties.toString() + ", but found " + propertyName);
+                            "for the second argument 'property.name' of getUserAgentProperty() function, " +
+                            "required one of " + listOfProperties.toString() + ", but found " + propertyName);
                 }
             } else {
-                throw new SiddhiAppValidationException("Parameter property.name of getUserAgentProperty() should be a" +
-                        " constant, but found a dynamic attribute.");
+                throw new SiddhiAppValidationException("Second parameter 'property.name' of getUserAgentProperty() " +
+                        "should be a constant, but found a dynamic attribute.");
             }
         } else {
             throw new SiddhiAppValidationException("Invalid no of arguments passed to " +
                     "env:getUserAgentProperty(user.agent,property.name) function, " +
                     "required 2, but found " + attributeExpressionExecutors.length);
         }
+        try {
+            uaParser = new Parser();
+        } catch (IOException e) {
+
+            throw new SiddhiAppCreationException("Failed to initiate user agent parser for the Siddhi app'" +
+                    siddhiAppContext.getName() + "'.", e);
+        }
+
     }
 
-    /**
-     * The main execution method which will be called upon event arrival
-     * when there are more than one function parameter.
-     *
-     * @param data the runtime values of function parameters.
-     * @return the function result.
-     */
     @Override
     protected Object execute(Object[] data) {
         String userAgent = (String) data[0];
-        String propertyName = ((String) data[1]).toLowerCase(Locale.ENGLISH);
-        String propertyValue;
-        try {
-            Parser parser = new Parser();
-            Client client = parser.parse(userAgent);
-            switch (propertyName) {
-                case UserAgentConstants.BROWSER:
-                    propertyValue = client.userAgent.family;
-                    break;
-                case UserAgentConstants.OPERATING_SYSTEM:
-                    propertyValue = client.os.family;
-                    break;
-                case UserAgentConstants.DEVICE:
-                    propertyValue = client.device.family;
-                    break;
-                default:
-                    propertyValue = null;
-                    break;
-            }
-        } catch (IOException e) {
-            throw new SiddhiAppRuntimeException("This event is dropped to an exception occurred while processing the " +
-                    "user agent.", e);
+        Client client = uaParser.parse(userAgent);
+        switch (propertyName.toLowerCase(Locale.ENGLISH)) {
+            case BROWSER:
+                return client.userAgent.family;
+            case OPERATING_SYSTEM:
+                return client.os.family;
+            case DEVICE:
+                return client.device.family;
+            // Default condition will never occur.
+            default:
+                return null;
         }
-        return propertyValue;
     }
 
-    /**
-     * The main execution method which will be called upon event arrival
-     * when there are zero or one function parameter.
-     *
-     * @param data null if the function parameter count is zero or
-     *             runtime data value of the function parameter.
-     * @return the function result.
-     */
     @Override
     protected Object execute(Object data) {
+        // This function is never reached.
         throw new SiddhiAppRuntimeException("Number of parameters passed to getUserAgentProperty() function " +
                 "is invalid");
     }
 
-    /**
-     * This will be called only once and this can be used to acquire
-     * required resources for the processing element.
-     * This will be called after initializing the system and before
-     * starting to process the events.
-     */
     @Override
     public Attribute.Type getReturnType() {
         return Attribute.Type.STRING;
     }
 
-    /**
-     * Used to collect the serializable state of the processing element, that need to be
-     * persisted for the reconstructing the element to the same state on a different point of time.
-     *
-     * @return stateful objects of the processing element as an array.
-     */
     @Override
     public Map<String, Object> currentState() {
         return null;
     }
 
-    /**
-     * Used to restore serialized state of the processing element, for reconstructing
-     * the element to the same state as if was on a previous point of time.
-     *
-     * @param state the stateful objects of the element as an array on the same order provided by currentState().
-     */
     @Override
     public void restoreState(Map<String, Object> state) {
 
